947159cea5e4983072c23d629e556a96c6b161ab
lavanya <lavanyabalaji1016@gmail.com>
2023-07-27 14:43:07 +0530

    login-commit2

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -13,12 +13,12 @@ function Navbar() {
                 <div className="navbar-light bg-light navbar-top">
                     <div className="Header container" >
                         <div className="row">
-                            <div className="col-lg-9 col-md-12 col-12  d-lg-inline">
+                            <div className="col-lg-9 col-md-12 col-12 d-lg-inline">
                                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                         <span className="navbar-toggler-icon"></span>
                                     </button>
-                                    {/* <div className="offcanvas offcanvas-end"  id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                                    <div className="offcanvas offcanvas-end"  id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                                         <div className="offcanvas-header">
                                             <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
                                             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -76,7 +76,8 @@ function Navbar() {
                                             </ul>
 
                                         </div>
-                                    </div> */}
+                                    </div>
+
                                     <div className="d-lg-none d-flex justify-content-center">
                                         <button className="nav-link navbtn">
                                             <i className="bi bi-search"></i>
@@ -113,7 +114,7 @@ function Navbar() {
                                 </nav>
                             </div>
                             <div className="col-lg-3 d-none d-lg-inline align-self-center">
-                                <div className="d-flex justify-content-lg-center justify-content-end">
+                                <div className="d-flex justify-content-lg-center justify-content-end ">
                                     <button className="nav-link navbtn"><Link to={"/Login"} className='text-decoration-none text-black'>Login</Link></button>
                                     <button className="nav-link navbtn">
                                         <i className="bi bi-search"></i>
